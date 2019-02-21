44b3af272331eed8936a75e3001431bf9043f1ce
kowvrettv <d3m11av@gmail.com>
2019-02-21 16:24:07 +0200

    css

--- project_mysite/settings.py
+++ project_mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4srzwe06wmfd7)j8y&d5%c#(3cp3)@u!v$2r!g1tr_srg^$!e2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
