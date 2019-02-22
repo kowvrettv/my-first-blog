1fbd127f7295f222c624dd194fb2cb1ebee5ff43
kowvrettv <d3m11av@gmail.com>
2019-02-22 17:23:20 +0200

    robots.txt

--- project_mysite/urls.py
+++ project_mysite/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import PostSitemap
 from django.conf.urls.static import static
@@ -31,4 +32,5 @@ urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
                         name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
